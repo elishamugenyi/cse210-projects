9f883b7ab9fe8dd0e89a59d0ff4afb933c4a4d8c
elishamugenyi <elishamugenyi5@gmail.com>
2023-09-13 02:34:00 +0300

    completed prepare1 with precise name program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine("What is your Last Name?");
+        string last_name = Console.ReadLine();
+
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
