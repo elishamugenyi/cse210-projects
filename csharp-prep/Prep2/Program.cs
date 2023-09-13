933c85fb48a33cbfc62ba1e65f3405926f03496c
elishamugenyi <elishamugenyi5@gmail.com>
2023-09-13 03:25:20 +0300

    did requirements 1&2, 3 was quite hard to grasp.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage, Do not include the percent symbol!");
+        string grade = Console.ReadLine();
+        int grade1 = int.Parse(grade);
+        
+        if (grade1 >= 90)
+        {
+            Console.WriteLine("Your Grade is A");
+        }
+        else if(grade1 >= 80)
+        {
+            Console.WriteLine("Your Grade is B");
+        }
+        else if(grade1 >= 70)
+        {
+            Console.WriteLine("Your Grade is C");
+        }
+        else if(grade1 >= 60)
+        {
+            Console.WriteLine("Your Grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your Grade is an F");
+        }
+
+        if (grade1== 70 || grade1>= 70)
+        {
+            Console.WriteLine("Congragulations, You passed the class");
+        }
+        else 
+        {
+            Console.Write("You did not pass the class, please try again next Semester");
+        }
     }
 }
