3794f0f134947d57499e205e8ca5f0aad7b81de1
elishamugenyi <elishamugenyi5@gmail.com>
2023-09-19 09:23:25 +0300

    did first step with if statements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        string number1 = Console.ReadLine();
+        int num = int.Parse(number1);
+
+        Console.WriteLine("What is your guess?");
+        string guess = Console.ReadLine();
+        int guessed = int.Parse(guess);
+
+        if (guessed <5)
+        {
+            Console.WriteLine("You guessed is lower");
+        }
+        else if(guessed > 5)
+        {
+            Console.WriteLine("You guessed is higher");
+        }
+        else 
+        {
+            Console.WriteLine("Correct, the Number is 5!");
+
+        }
+
     }
 }
