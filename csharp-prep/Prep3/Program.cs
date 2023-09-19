c173a6acffe146fb5a4d1372045da4e9b3f34daf
elishamugenyi <elishamugenyi5@gmail.com>
2023-09-19 11:02:12 +0300

    changed the last part and added random generator

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,25 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
+        //this is for part 1
         Console.WriteLine("What is the magic number?");
-        string number1 = Console.ReadLine();
-        int num = int.Parse(number1);
+       // string number1 = Console.ReadLine();
+        //int num = int.Parse(number1);
 
-        Console.WriteLine("What is your guess?");
-        string guess = Console.ReadLine();
-        int guessed = int.Parse(guess);
+        //Console.WriteLine("What is your guess?");
+        //string guess = Console.ReadLine();
+       // int guessed = int.Parse(guess);
 
-        while (guessed <5)
+        //this is part 2 where i introduced a while loop.
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1,100);
+
+        int guess = -1;
+
+
+        while (guess != num)
         {
-            Console.WriteLine("Lower, Try again");
-            Console.ReadLine();
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
         }
-        while (guessed > 5)
-        {
-            Console.WriteLine("Higher, Try again");
-            Console.ReadLine();
-        }
-        Console.WriteLine("Correct, the Number is 5!");
-
+            if (num > guess)
+            {
+                Console.WriteLine("Higher, Try again");
+            }
+            else if (num < guess)
+            {
+                Console.WriteLine("Lower, Try again");
+            }
+            else 
+            {
+            Console.WriteLine("Correct, congrats!");
+            }
     }
 }
