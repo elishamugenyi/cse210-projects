78f7f7842480b9ef39741eb6eaa4ee1527546b11
elishamugenyi <elishamugenyi5@gmail.com>
2023-10-13 02:49:48 +0300

    added objects for different numbers

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.WriteLine("Welcome to Encapsulation process.!");
+        //create fractions using different constructors
+
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"fraction1 is:{fraction1.GetFractionString()}");
+        Console.WriteLine($"fraction1 is:{fraction1.GetDecimalValue()}");
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine($"fraction2 is:{fraction2.GetFractionString()}");
+        Console.WriteLine($"fraction2 is:{fraction2.GetDecimalValue()}");
+
+        Fraction fraction3 = new Fraction(5,3);
+        Console.WriteLine($"For fraction 3 {fraction3.GetFractionString()}");
+        Console.WriteLine($"For fraction 3 {fraction3.GetDecimalValue()}");
+    
     }
 }
