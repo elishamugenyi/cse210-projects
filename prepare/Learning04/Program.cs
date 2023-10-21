a8057e092afa69a5c8263a2d321b1eb197d99b95
elishamugenyi <elishamugenyi5@gmail.com>
2023-10-21 20:13:53 +0300

    created instances and called the methods

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.ComponentModel;
+using System.Reflection.PortableExecutable;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Elisha", "Inheritance");
+          
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment m1 = new MathAssignment("Elisha", "Inheritance", "OOP", "Abstraction");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(m1.GetHomeWorkList());
+
+        WritingAssignment w1 = new WritingAssignment("Elisha", "Wealth", "Rich Dad Poor Dad");
+        //Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(w1.GetWritingInfo());
     }
 }
