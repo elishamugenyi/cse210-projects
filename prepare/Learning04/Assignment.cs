e22c32a21679f3dd6006f124d7471c28fcf21ad5
elishamugenyi <elishamugenyi5@gmail.com>
2023-10-21 20:12:56 +0300

    created it as base class

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,37 @@
+using System;
+
+//create a base/parent/super class with attribrutes needed by other child classes.
+
+public class Assignment
+{
+    //all attributes here are private (encapsulation principle)
+
+    private string _studentname;
+    private string _topic;
+   
+
+    //create a no argument constructor
+    public Assignment(string studentName, string topic)
+    {
+        _studentname = studentName;
+        _topic = topic;
+
+    }
+
+    //create get methods
+    public string GetStudentName()
+    {
+        return _studentname;
+    }
+    public string GetTopic()
+    {
+        return _topic;
+    }
+
+    //create a method GetSummary
+    public string GetSummary()
+    {
+        return _studentname + "-" + _topic;
+    }
+
+}
