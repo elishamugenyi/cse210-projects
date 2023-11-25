4b6e891b91e2d95c5a2043cc679fed461bed79d8
elishamugenyi <elishamugenyi5@gmail.com>
2023-11-26 01:52:39 +0300

    testing student class

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        Student student = new Student();
+        student.SignUp();
+        Console.WriteLine("Hello FinalProject World!...The End");
     }
 }
