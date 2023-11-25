5b0d74f13663716a379e430236e0e8923ab36872
elishamugenyi <elishamugenyi5@gmail.com>
2023-11-26 02:25:14 +0300

    created a login class

---
+++ final/FinalProject/Login.cs
@@ -0,0 +1,14 @@
+using System;
+using OfficeOpenXml;
+using System.IO;
+
+//create a login class that picks student ID and password from the studentdata.xlsx file and logs in student.
+public class Login : Student
+{
+    private string _username;
+    //to be continued
+    public void LoginStudent()
+    {
+        //to be continued on monday, going to sleep now
+    }
+}
