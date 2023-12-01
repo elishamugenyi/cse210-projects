0426348c6e27c94766ede8cac281d1ca24205b2f
elishamugenyi <elishamugenyi5@gmail.com>
2023-12-02 01:40:22 +0300

    added the transaction function inside the class method

--- final/FinalProject/PayUniversity.cs
+++ final/FinalProject/PayUniversity.cs
@@ -1,7 +1,12 @@
 //this does not inherit from student but use student as an object because paying is an activity and not some type of student.
+using OfficeOpenXml;
+using OfficeOpenXml.Filter;
 using System;
+using System.Data.Common;
 using System.IO;
-using System.Runtime.InteropServices;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
 
 //create a class for paying, does not inherit from student because it is an action not a typenof student
 public class PayUniversity
@@ -23,14 +28,37 @@ public class PayUniversity
         if (student.AccountBalance == 0)
         {
             TopUp top = new TopUp();
-            top.TopUp(student)
+            top.BuyCredit(student);
             Console.WriteLine("Enter Tuition Amount to Pay:");
             _tuitionamount = double.Parse(Console.ReadLine());
             student.AccountBalance -= _tuitionamount;
 
-            Console.WriteLine("");
-            //Console.WriteLine($"Your new Account Balance is: {student.AccountBalance}");
+            //Console.WriteLine("");
             Console.WriteLine("Paid successfully");
+            Console.WriteLine($"Your new Account Balance is: {student.AccountBalance}");
+
+            Console.WriteLine("Your transactions!");
+        
+            //create a file that stores student credit & semester payment transactions for remitances in the future
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage())
+            {
+                //add worksheet to store these details to the excel package
+                var worksheet = package.Workbook.Worksheets.Add("StudentTransactions");
+
+                //set column headers
+                worksheet.Cells["E1"].Value = "Semester";
+                worksheet.Cells["F1"].Value = "Tuition Paid";
+
+                //add values to the excel sheet
+                worksheet.Cells["E2"].Value = _semester;
+                worksheet.Cells["F2"].Value = _tuitionamount;
+                //save the file
+                var file = new FileInfo("StudentTransactions.xlsx");
+                package.SaveAs(file);
+            }
+            Console.WriteLine("Transactions saved.");
+            
             //student.AccountBalance -= _tuitionamount;
             //Console.WriteLine("Paid successfully!");
             
